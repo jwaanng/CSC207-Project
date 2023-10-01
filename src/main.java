9e8e647f97edf95928d3101fd2a4e9c9b4b5b19b
Mingyoung Lai <mingyounglai@mingyoungs-mbp.home>
2023-10-01 00:38:36 -0400

    add directory

---
+++ src/main.java
@@ -0,0 +1,19 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("https://api.thecatapi.com/v1/images/search?format=json&limit=10")
+                .addHeader("Content-Type", "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        String responsebody = response.body().string();
+        System.out.println(responsebody);
+    }
+}

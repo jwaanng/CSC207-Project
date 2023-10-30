9e71ab1237161ca0a641114ab258eda9176647fb
hanhhieudao <hanhdaohieu@gmail.com>
2023-10-30 11:39:32 -0400

    add Comparator

---
+++ src/Entity/Comparator/DogProfileComparatorBySize.java
@@ -0,0 +1,15 @@
+package Entity.Comparator;
+
+import Entity.PetProfiles.DogProfile;
+
+import java.util.Comparator;
+
+class DogProfileComparatorBySize implements Comparator<DogProfile> {
+    @Override
+    public int compare(DogProfile p1, DogProfile p2) {
+        return 0;
+    }
+    public Comparator<DogProfile> thenComparing(Comparator<? super DogProfile> comparator) {
+        return null;
+    }
+}

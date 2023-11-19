3b83bd2c145da48b08dd6f70241a583a039eb7fb
Ming593 <ming.lai@mail.utoronto.ca>
2023-11-19 00:59:36 -0500

    add login usecase, interfaceadapters, view

--- src/BePetOwner/BPODAI.java
+++
@@ -1,9 +0,0 @@
-package BePetOwner;
-
-import Entity.User.AppUser;
-
-public interface BPODAI {
-    void save(AppUser user);
-    AppUser retrieve(String username);
-
-}
---
+++ src/View/ViewManager.java
@@ -0,0 +1,8 @@
+package ViewModel;
+
+import java.beans.PropertyChangeSupport;
+
+public class ViewManager {
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    
+}

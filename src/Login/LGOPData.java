04eb2bb2fc79d83f168759aae656c13f221defc7
Ming593 <ming.lai@mail.utoronto.ca>
2023-11-19 15:42:50 -0500

    wrong file last time login folder push

--- src/Login/LGOPData.java
+++ src/Login/LGOPData.java
@@ -1,2 +1,51 @@
-package Login;public class LGOPData {
+package Login;
+
+import Entity.User.AppUser;
+
+import java.util.HashMap;
+
+public class LGOPData {
+    private String username;
+    private boolean usecaseFailed;
+
+    private final HashMap<String, String> petName_to_photo = new HashMap<>();
+    private String error;
+    //TODO need to contain all relevant for petProfile display
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isUsecaseFailed() {
+        return usecaseFailed;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public LGOPData(LGOPData copy){
+        this.username = copy.username;
+        this.error = copy.error;
+        this.usecaseFailed = copy.usecaseFailed;
+    }
+    public LGOPData(){}
+
+    public static LGOPData createSuccessData(String username)
+    {
+        LGOPData data = new LGOPData();
+        data.username = username;
+        return data;
+    }
+    public static LGOPData createFailData(String error){
+        LGOPData data = new LGOPData();
+        data.error = error;
+        data.usecaseFailed = true;
+        return data;
+    }
+    public void addPetPHOTO(String petName, String photoUrl){
+         petName_to_photo.put(petName, photoUrl);
+    }
+    public HashMap<String, String> getPetNames_Photos_Map(){
+        return new HashMap<String, String>(petName_to_photo);
+    }
 }
--- src/Login/LGPresenter.java
+++ src/Login/LGPresenter.java
@@ -1,2 +1,44 @@
-package Login;public class LGPresenter {
+package Login;
+
+import FavPetPage.FavPetPageState;
+import FavPetPage.FavPetViewModel;
+import ViewModel.ViewModelManager;
+
+public class LGPresenter implements LGOB{
+    private final LGViewModel lgvm;
+    private final FavPetViewModel pvm;
+
+
+    private final ViewModelManager manager;
+
+
+    public LGPresenter(LGViewModel loginViewModel, FavPetViewModel myFavPetPageViewModel, ViewModelManager manager) {
+        this.lgvm = loginViewModel;
+        this.manager = manager;
+        this.pvm = myFavPetPageViewModel;
+    }
+
+
+    @Override
+    public void prepareSuccessView(LGOPData outputData) {
+        //login will automatically be redirected to the favPetPage
+        //precondition outputData is of type success data
+        String username = outputData.getUsername();
+        FavPetPageState state = new FavPetPageState(outputData);
+        pvm.setState(state); //viewModel now contain information about the user's name
+        pvm.firePropertyChanged();
+        manager.setActiveViewName(pvm.getViewName()); //redirect to myPetPage through manager
+        manager.firePropertyChange();
+
+
+    }
+
+    @Override
+    public void prepareFailView(LGOPData outdata) {
+            LGState state = new LGState();
+            state.setError(outdata.getError());
+            lgvm.setState(state);
+            lgvm.firePropertyChanged();
+    }
 }
+

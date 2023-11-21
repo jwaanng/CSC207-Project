818d639b5880b9f124def5fd863c910d21ac259f
Sean Kong <x.kong@mail.utoronto.ca>
2023-11-21 16:35:57 -0500

    added SwipePageView files

---
+++ src/View/SwipePageView.java
@@ -0,0 +1,11 @@
+package View;
+
+import javax.swing.*;
+import javax.swing.ButtonGroup;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwipePageView {
+
+}

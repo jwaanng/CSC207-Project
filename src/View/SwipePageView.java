adb0f13eab32b98796e6c3f23ea23787a3e81f7c
Sean Kong <x.kong@mail.utoronto.ca>
2023-11-24 23:02:46 -0500

    nothing new just re did some stuff

--- src/View/SwipePageView.java
+++ src/View/SwipePageView.java
@@ -6,6 +6,16 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SwipePageView {
+public class SwipePageView extends JFrame {
+    public SwipePageView(){
+        setTitle("Find a pet!");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 450);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(SwipePageView::new);
+    }
 }

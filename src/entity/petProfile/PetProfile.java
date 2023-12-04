5819965aa5a9babc05864c05e7d5878638b8d032
Sean Kong <x.kong@mail.utoronto.ca>
2023-12-04 02:02:37 -0500

    test

--- src/entity/petProfile/PetProfile.java
+++ src/entity/petProfile/PetProfile.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
  */
 public abstract class PetProfile {
     public static final String MALE = "Male";
+    private String str;
 
     /**
      * Identifier for female pets.

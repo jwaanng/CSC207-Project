6f1258ae103a5552e6a9643ff12ef6be2b076219
Ming593 <ming.lai@mail.utoronto.ca>
2023-12-04 02:44:31 -0500

    add browsepage redirectm, change app progrmamming

---
+++ src/browsePage/swipePageRedirect/BrowsePageRDRPresenter.java
@@ -0,0 +1,19 @@
+package browsePage.swipePageRedirect;
+
+import viewModel.ViewModelManager;
+
+public class BrowsePageRDRPresenter implements BrowsePageRDROB {
+    private final ViewModelManager manager;
+    private final BrowsePageViewModel browsePageVM;
+
+    public BrowsePageRDRPresenter(ViewModelManager manager, BrowsePageViewModel browsePageViewModel) {
+        this.manager = manager;
+        this.browsePageVM = browsePageViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView() {
+        manager.setActiveViewName(browsePageVM.getViewName());
+        manager.firePropertyChange();
+    }
+}
